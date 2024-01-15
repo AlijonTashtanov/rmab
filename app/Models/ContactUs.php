0ab4fdc1a71968bfd2a96f839f333a18ee12f75a
Ulugbek-Muhammadjonov <muhammadjonovulugbek98@gmail.com>
2024-01-15 16:26:37 +0500

    feat: quality control, crud, api, migration

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -3,18 +3,38 @@
 namespace App\Models;
 
 use App\Traits\Status;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class ContactUs extends Model
 {
     use Status;
 
+
+    /**
+     * @var array
+     */
     protected $fillable = [];
 
+    /**
+     * @param $search
+     * @return Builder
+     */
     public static function search($search)
     {
         return empty($search)
             ? static::query()
             : static::query()->where('full_name', 'like', '%' . $search . '%');
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function statuses()
+    {
+        return [
+            self::$status_active => "O'qildi",
+            self::$status_inactive => "O'qilmadi"
+        ];
     }
 }
