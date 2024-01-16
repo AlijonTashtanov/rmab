14e15bfe2041e5b0dc00f27f693f24f974b25d28
Ulugbek-Muhammadjonov <muhammadjonovulugbek98@gmail.com>
2024-01-16 13:32:35 +0500

    feat: add social crud,api,migration,service

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ApplicationApplicant;
+use App\Models\ApplicationUseService;
+use App\Models\ContactUs;
+use App\Models\ContractConclusion;
+use App\Models\InformationAboutShipment;
+use App\Models\QualityControl;
+use App\Models\Vacancy;
+use App\Traits\Status;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Cookie;
 
 class AdminController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
-        return view('admin.index');
+        $vacancyCount = Vacancy::count();
+
+        $inactiveApplicationApplicantsCount = ApplicationApplicant::where('status', Status::$status_inactive)
+            ->count();
+
+        $inactiveContractConclusionCount = ContractConclusion::where('status', Status::$status_inactive)
+            ->count();
+
+        $inactiveQualityControlCount = QualityControl::where('status', Status::$status_inactive)
+            ->count();
+
+        $inactiveInformationAboutShipmentCount = InformationAboutShipment::where('status', Status::$status_inactive)
+            ->count();
+
+        $inactiveApplicationUseServiceCount = ApplicationUseService::where('status', Status::$status_inactive)
+            ->count();
+
+        $inactiveContactUseCount = ContactUs::where('status', Status::$status_inactive)
+            ->count();
+
+        return view('admin.index', [
+            'vacancyCount' => $vacancyCount,
+            'inactiveApplicationApplicantsCount' => $inactiveApplicationApplicantsCount,
+            'inactiveContractConclusionCount' => $inactiveContractConclusionCount,
+            'inactiveQualityControlCount' => $inactiveQualityControlCount,
+            'inactiveInformationAboutShipmentCount' => $inactiveInformationAboutShipmentCount,
+            'inactiveApplicationUseServiceCount' => $inactiveApplicationUseServiceCount,
+            'inactiveContactUseCount' => $inactiveContactUseCount
+        ]);
     }
 
     public function setCookie($cookie)
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,5 +3,80 @@
     Admin dashboard
 @endsection
 @section('content')
-    admin dashboard
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-info elevation-1"><a href="{{route('admin.vacancies.index')}}"><i
+                        class="fas fa-user-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Vakansiyalar</span>
+                    <span class="info-box-number">{{$vacancyCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-danger elevation-1"><a
+                    href="{{route('admin.applicationapplicants.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">O'qilmagan ariza va murojaatlar</span>
+                    <span class="info-box-number">{{$inactiveApplicationApplicantsCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-yellow elevation-1"><a href="{{route('admin.contractconclusions.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">O'qilmagan shartnoma tuzuvchilar</span>
+                    <span class="info-box-number">{{$inactiveContractConclusionCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-success elevation-1"><a href="{{route('admin.qualitycontrols.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">O'qilmagan sifat nazorati</span>
+                    <span class="info-box-number">{{$inactiveQualityControlCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-secondary elevation-1"><a
+                    href="{{route('admin.informationaboutshipments.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Jo’natma haqida ma’lumot olmoqchilar</span>
+                    <span class="info-box-number">{{$inactiveInformationAboutShipmentCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-white elevation-1"><a
+                    href="{{route('admin.applicationuseservices.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Xizmatlardan foydalanish uchun ariza berganlar</span>
+                    <span class="info-box-number">{{$inactiveApplicationUseServiceCount}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box shadow">
+            <span class="info-box-icon bg-indigo elevation-1"><a
+                    href="{{route('admin.contactuses.index')}}"><i
+                        class="fas fa-circle"></i></a></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">O'qilmagan savol va takliflar</span>
+                    <span class="info-box-number">{{$inactiveContactUseCount}}</span>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
